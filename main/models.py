2fbaa64a385bc26d9d01ee88e3560313f28e8cb9
It future <>
2022-11-22 16:56:56 +0500

    likes

--- main/models.py
+++ main/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=255, verbose_name="Название статьи")
     content = models.TextField(verbose_name="Текст статьи")
+    likes = models.PositiveIntegerField(verbose_name="лайки", default=0)
 
     def __str__(self):
         return self.title
