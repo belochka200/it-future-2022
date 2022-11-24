8e13eb8b2d3cbf126f1792b1e2b10a0ad1348f29
It future <>
2022-11-24 16:03:26 +0500

    navbar + url for cards

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', home),
+    path('', home, name='home'),
     path('user/', test),
-    path('post/<int:pk>/', показать_пост,)
+    path('post/<int:pk>/', показать_пост, name='show_post')
 ]
